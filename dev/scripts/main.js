5adc162bf58f8cb5339b3245f2cdf607e8cc1863
Emma Kinch <eokinch@gmail.com>
2017-09-18 12:24:07 -0400

    initial push of files

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,48 @@
-const ohHey = "Hello World";
+const navFunction = function() {
+	const nav = $('.header-nav');
+	const navIcon = $('.mobile-nav');
+	const navItem = $('.header-nav li a');
+	let navOpen = false;
 
-console.log(ohHey);
+	function openNav() {
+		nav.css('left', '0px');
+		navOpen = true;
+	}
+
+	function closeNav() {
+		nav.css('left', '-200px');
+		navOpen = false;
+	}
+
+	function toggleNav() {
+		if(navOpen === true) {
+			closeNav();
+		}else {
+			openNav();
+		}
+	}
+
+	navItem.on('click', function(){
+		closeNav();
+	})
+
+	navIcon.on('mouseenter', function(){
+		openNav();
+	});
+
+	navIcon.on('click', function(){
+		toggleNav();
+	});
+
+	nav.on('mouseleave', function(){
+		closeNav();
+	});
+
+	window.onscroll = function() {
+		closeNav();
+	}
+}
+
+$(function(){
+	navFunction();
+});
